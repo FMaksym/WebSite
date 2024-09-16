17d383a87115bccfb28040a88ff70d2ad3297556
FMaksym <max05filipev@gmail.com>
2024-09-16 18:19:12 +0300

    Change site

--- Models/FormData.cs
+++
@@ -1,8 +0,0 @@
-﻿public class FormData
-{
-    public string Name { get; set; }
-    public string Company { get; set; }
-    public string Email { get; set; }
-    public string Phone { get; set; }
-    public string OrderDescription { get; set; }
-}
